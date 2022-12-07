c7377e6f58395649abba2026f10490cd8524f5c1
kStasova <stasovakatka6688@gmail.com>
2022-12-07 12:28:46 +0100

    fix: run lint

--- src/components/movie-detail-view/PosterDetection.vue
+++ src/components/movie-detail-view/PosterDetection.vue
@@ -28,7 +28,6 @@ export default defineComponent({
   computed: {
     ...mapState("movie", ["movie"]),
   },
-
   methods: {
     detect() {
       let canvas = this.$refs["myCanvas"] as HTMLCanvasElement;
